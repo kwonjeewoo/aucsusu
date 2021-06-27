11156c91f975ca8d95284585d68297c20602caa9
kwonjeewoo <jcloud1122@naver.com>
2021-06-27 11:46:08 +0900

    first commit

---
+++ src/main/java/com/yonamz/aucsusu/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.yonamz.aucsusu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/")
+    public String getRequest(){
+        return "index";
+    }
+}
